e3b675c689ae5796da66d85d6fadfb14137fb9b1
BakaNoCrimson <Sidney.nguyen@live.fr>
2024-07-15 11:46:00 +0200

    :art: improve CSS

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,15 +5,19 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      
       <nav>
-      <img src="src\Components\Assets\logo.png" alt="Logo" />
+        <img src="src\\Components\\Assets\\logo.png" alt="Logo" />
         <a href="#">Accueil</a>
         <a href="#">Mes films</a>
         <a href="#">Mes séries</a>
       </nav>
       <div className="search">
-        <input type="text" placeholder="Rechercher..." />
+        <form action="" className="search-bar">
+          <input type="search" name="search" pattern=".*\S.*" required />
+          <button className="search-btn" type="submit">
+            <span>Search</span>
+          </button>
+        </form>
         <div className="profile"></div>
       </div>
     </header>
