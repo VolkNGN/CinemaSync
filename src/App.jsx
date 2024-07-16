010b9d7aa6669588b7346a30600df259ac5d0be6
AlexAequo <suzuone01@gmail.com>
2024-07-16 09:52:17 +0200

    :sparkles: page profil router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 // src/App.jsx
-import React, { useState } from 'react';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import FavoriteMovies from './Components/FavoriteMovies';
 import MovieList from './Components/MovieList';
+import ProfilePage from './Components/profil/ProfilePage';
+import NotFoundPage from './Components/Notfound/NotFoundPage';  // Mise à jour du chemin
 import './App.css';
 
 const App = () => {
@@ -10,29 +13,40 @@ const App = () => {
   const apiKey = '7b45af8f71d63d716fa486d7d0abb8bd';
 
   const addFavoriteMovie = async (movieId) => {
-    const response = await fetch(`https://api.themoviedb.org/3/account/${accountId}/favorite?api_key=${apiKey}&session_id=${sessionId}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        media_type: 'movie',
-        media_id: movieId,
-        favorite: true
-      })
-    });
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/account/${accountId}/favorite?api_key=${apiKey}&session_id=${sessionId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          media_type: 'movie',
+          media_id: movieId,
+          favorite: true
+        })
+      });
 
-    if (response.ok) {
-      console.log(`Movie ${movieId} added to favorites`);
+      if (response.ok) {
+        console.log(`Movie ${movieId} added to favorites`);
+      } else {
+        console.error('Failed to add movie to favorites');
+      }
+    } catch (error) {
+      console.error('Error:', error);
     }
   };
 
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<div>Page d'accueil</div>} />
+        <Route path="/favorites" element={<FavoriteMovies accountId={accountId} sessionId={sessionId} apiKey={apiKey} />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
       <MovieList addFavoriteMovie={addFavoriteMovie} />
-      <FavoriteMovies accountId={accountId} sessionId={sessionId} apiKey={apiKey} />
     </div>
   );
-}
+};
 
 export default App;
