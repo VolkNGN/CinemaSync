2e551d4b0e733f56a3d3f884da9f1ddb35f9e50e
AlexAequo <suzuone01@gmail.com>
2024-07-17 14:57:10 +0200

    :tada:  Footer Ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import React from 'react'; // Importation de React
-import { Routes, Route } from 'react-router-dom'; // Importation des composants de routage
-import Header from './Components/Header/Header'; // Importation du composant Header
-import FavoriteMovies from './Components/FavoriteMovies'; // Importation du composant FavoriteMovies
-import MovieList from './Components/MovieList'; // Importation du composant MovieList
-import ProfilePage from './Components/profil/ProfilePage'; // Importation du composant ProfilePage
-import SuggestionsCarousel from './Components/SuggestionsCarousel/SuggestionsCarousel'; // Importation du composant SuggestionsCarousel
-import './App.css'; // Importation des styles de l'application
+// src/App.jsx
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header/Header';
+import FavoriteMovies from './Components/FavoriteMovies';
+import ProfilePage from './Components/profil/ProfilePage';
+import SuggestionsCarousel from './Components/SuggestionsCarousel/SuggestionsCarousel';
+import Footer from './Components/Footer/Footer'; // Importez le composant Footer ici
+import './App.css';
 
 const App = () => {
-  const tmdbApiKey = '7b45af8f71d63d716fa486d7d0abb8bd'; // Clé API TMDB
-  const youtubeApiKey = 'AIzaSyDIGqLiH3uOgyP3ZekG62cGXWSs-HvsLRs'; // Clé API YouTube IFrame
+    const tmdbApiKey = '7b45af8f71d63d716fa486d7d0abb8bd'; // Clé API TMDB
+    const youtubeApiKey = 'AIzaSyDIGqLiH3uOgyP3ZekG62cGXWSs-HvsLRs'; // Clé API YouTube IFrame
 
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={
-          <div>
-            {/* <h1>Page d'accueil</h1> */}
-            <SuggestionsCarousel tmdbApiKey={tmdbApiKey} youtubeApiKey={youtubeApiKey} />
-          </div>
-        } />
-        <Route path="/favorites" element={<FavoriteMovies />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-    </div>
-  );
+    return (
+        
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<SuggestionsCarousel tmdbApiKey={tmdbApiKey} youtubeApiKey={youtubeApiKey} />} />
+                    <Route path="/favorites" element={<FavoriteMovies />} />
+                    <Route path="/profile" element={<ProfilePage />} />
+                </Routes>
+                <Footer />
+            </div>
+        
+    );
 };
 
 export default App;
